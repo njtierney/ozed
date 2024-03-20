2e2b677cd120947ea3c2d8959e00464975019297
njtierney <nicholas.tierney@gmail.com>
2024-03-20 22:17:02 +1000

    update script 02 and 0201

---
+++ script_00_2_1.R
@@ -0,0 +1,11 @@
+source("packages.R")
+## Load your R files
+lapply(list.files("./R", full.names = TRUE), source)
+
+educated_population_2014 <- read_educated_population(year = "2014")
+
+ggplot(educated_population_2014,
+       aes(x = prop_studying,
+           y = state_territory)) + 
+  geom_col() + 
+  facet_wrap(~age_group)
--- script_02.R
+++ script_02.R
@@ -1,6 +1,19 @@
-source("packages.R")
+library(tidyverse)
+library(here)
+library(readxl)
+library(naniar)
+library(visdat)
+library(janitor)
 ## Load your R files
-lapply(list.files("./R", full.names = TRUE), source)
+source("R/combine_educated_population.R")
+source("R/extract_education_col_names.R")
+source("R/extract_rows_set_names.R")
+source("R/pivot_longer_educated.R")
+source("R/pivot_longer_population.R")
+source("R/prepare_education.R")
+source("R/prepare_population.R")
+source("R/read_educated_population.R")
+source("R/read_raw_education_data.R")
 
 educated_population_2014 <- read_educated_population(year = "2014")
 
